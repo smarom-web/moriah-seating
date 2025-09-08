90e451c8831c23ec600cb06f797f099e5614a81c
Shimi <smarom@gmail.com>
2025-09-08 16:44:57 -0500

    URL Magic Link Update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2022'      // or 'esnext'
+  },
+  esbuild: {
+    target: 'es2022'      // keep esbuild in sync
+  }
 })
